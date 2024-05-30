24f06b42d79643a26fb8f592358e85338d285846
rbrenor2 <rbrenorios@gmail.com>
2024-05-30 09:40:18 +0100

    Initial Commit

---
+++ nobullshit/nobullshitApp.swift
@@ -0,0 +1,17 @@
+//
+//  nobullshitApp.swift
+//  nobullshit
+//
+//  Created by Breno R R on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct nobullshitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
